e969eb9531c125705039eadb68fef54ed73e7a26
ONordander <otto.nordander@gmail.com>
2025-05-01 13:38:21 +0200

    Add README.md

--- rules/index.go
+++ rules/index.go
@@ -7,7 +7,7 @@ import (
 var indexingRules = []Rule{
 	{
 		Code:     "non-concurrent-index-creation",
-		Slug:     "Creating an index takes a lock on the table that block writes to the table from the application while the index is being built",
+		Slug:     "Creating an index non-concurrently acquires a lock on the table that block writes while the index is being built",
 		Help:     "Build the index concurrently to avoid blocking. Note: this cannot be done inside a transaction",
 		Fn:       nonConcurrentIndexCreation,
 		Category: indexing,
