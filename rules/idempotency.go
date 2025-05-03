26d52406908fd353d95d9ea960514f36b4980132
ONordander <otto.nordander@gmail.com>
2025-05-03 13:16:09 +0200

    rule: merge missing-index-if-not-exists with missing-relation-if-not-exists

--- rules/idempotency.go
+++ rules/idempotency.go
@@ -6,52 +6,44 @@ import (
 
 var idempotencyRules = []Rule{
 	{
-		Code:     "missing-relation-if-not-exists",
-		Slug:     "Creating a relation will fail if it already exists, making the migration non idempotent",
+		Code:     "missing-if-not-exists",
+		Slug:     "Creating an object might fail if it already exists, making the migration non idempotent",
 		Help:     "Wrap the create statements with guards; e.g. CREATE TABLE IF NOT EXISTS pgcheck ...",
-		Fn:       missingRelationIfNotExists,
-		Category: idempotency,
-	},
-	{
-		Code:     "missing-index-if-not-exists",
-		Slug:     "Creating a named index will fail if it already exists, making the migration non idempotent",
-		Help:     "Wrap the create statements with guards; e.g. CREATE INDEX CONCURRENTLY IF NOT EXISTS pgcheck_idx ...",
-		Fn:       missingIndexIfNotExists,
+		Fn:       missingIfNotExists,
 		Category: idempotency,
 	},
 }
 
-func missingRelationIfNotExists(tree *pgquery.ParseResult, code Code, slug, help string) ([]Result, error) {
+func missingIfNotExists(tree *pgquery.ParseResult, code Code, slug, help string) ([]Result, error) {
 	var results []Result
-	for _, decl := range FilterStatements[*pgquery.Node_CreateStmt](tree.Stmts) {
-		if !decl.Stmt.CreateStmt.IfNotExists {
-			r := Result{
-				Slug:      slug,
-				Help:      help,
-				Code:      code,
-				StmtStart: decl.Start,
-				StmtEnd:   decl.End,
+	for _, stmt := range tree.Stmts {
+		// Check relation creations
+		if createStmt := stmt.GetStmt().GetCreateStmt(); createStmt != nil {
+			if !createStmt.IfNotExists {
+				r := Result{
+					Slug:      slug,
+					Help:      help,
+					Code:      code,
+					StmtStart: stmt.GetStmtLocation(),
+					StmtEnd:   stmt.GetStmtLocation() + stmt.GetStmtLen(),
+				}
+				results = append(results, r)
 			}
-			results = append(results, r)
 		}
-	}
 
-	return results, nil
-}
-
-func missingIndexIfNotExists(tree *pgquery.ParseResult, code Code, slug, help string) ([]Result, error) {
-	var results []Result
-	for _, decl := range FilterStatements[*pgquery.Node_IndexStmt](tree.Stmts) {
-		isNamedIndex := decl.Stmt.IndexStmt.Idxname != ""
-		if !decl.Stmt.IndexStmt.IfNotExists && isNamedIndex {
-			r := Result{
-				Slug:      slug,
-				Help:      help,
-				Code:      code,
-				StmtStart: decl.Start,
-				StmtEnd:   decl.End,
+		// Check index creations
+		if indexStmt := stmt.GetStmt().GetIndexStmt(); indexStmt != nil {
+			isNamedIndex := indexStmt.Idxname != ""
+			if !indexStmt.IfNotExists && isNamedIndex {
+				r := Result{
+					Slug:      slug,
+					Help:      help,
+					Code:      code,
+					StmtStart: stmt.GetStmtLocation(),
+					StmtEnd:   stmt.GetStmtLocation() + stmt.GetStmtLen(),
+				}
+				results = append(results, r)
 			}
-			results = append(results, r)
 		}
 	}
 
--- rules/idempotency_test.go
+++ rules/idempotency_test.go
@@ -8,16 +8,16 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestMissingRelationIfNotExists(t *testing.T) {
+func TestMissingIfNotExists(t *testing.T) {
 	t.Parallel()
 
-	t.Run("Should find violation", func(t *testing.T) {
+	t.Run("Should find violation for relation", func(t *testing.T) {
 		t.Parallel()
 
 		tree := mustParse(t, "CREATE TABLE pgcheck (id integer PRIMARY KEY);")
 		require.Len(t, tree.Stmts, 1)
 
-		res, err := missingRelationIfNotExists(tree, testCode, testSlug, testHelp)
+		res, err := missingIfNotExists(tree, testCode, testSlug, testHelp)
 		require.NoError(t, err)
 		require.Len(t, res, 1)
 
@@ -28,62 +28,13 @@ func TestMissingRelationIfNotExists(t *testing.T) {
 		assert.EqualValues(t, testHelp, res[0].Help)
 	})
 
-	t.Run("Should find multiple violations", func(t *testing.T) {
-		t.Parallel()
-
-		var b strings.Builder
-		b.WriteString("CREATE TABLE pgcheck (id integer PRIMARY KEY);")
-		b.WriteString("ALTER TABLE pgcheck RENAME COLUMN value TO new_value;\n")
-		b.WriteString("CREATE TABLE pgcheck_two (id integer PRIMARY KEY);")
-		tree := mustParse(t, b.String())
-		require.Len(t, tree.Stmts, 3)
-
-		res, err := missingRelationIfNotExists(tree, testCode, testSlug, testHelp)
-		require.NoError(t, err)
-		require.Len(t, res, 2)
-
-		assert.EqualValues(t, 0, res[0].StmtStart)
-		assert.EqualValues(t, 99, res[1].StmtStart)
-	})
-
-	t.Run("Should not return statements that are safe", func(t *testing.T) {
-		t.Parallel()
-
-		var b strings.Builder
-		b.WriteString("CREATE TABLE IF NOT EXISTS pgcheck (id integer PRIMARY KEY);")
-		tree := mustParse(t, b.String())
-		require.Len(t, tree.Stmts, 1)
-
-		res, err := missingRelationIfNotExists(tree, testCode, testSlug, testHelp)
-		require.NoError(t, err)
-		assert.Empty(t, res)
-	})
-
-	t.Run("Should find no violations", func(t *testing.T) {
-		t.Parallel()
-
-		var b strings.Builder
-		b.WriteString("ALTER TABLE pgcheck RENAME COLUMN value TO new_value;\n")
-		b.WriteString("ALTER TABLE pgcheck RENAME COLUMN value TO new_value;\n")
-		tree := mustParse(t, b.String())
-		require.Len(t, tree.Stmts, 2)
-
-		res, err := missingRelationIfNotExists(tree, testCode, testSlug, testHelp)
-		require.NoError(t, err)
-		assert.Empty(t, res)
-	})
-}
-
-func TestMissingIndexIfNotExists(t *testing.T) {
-	t.Parallel()
-
-	t.Run("Should find violation", func(t *testing.T) {
+	t.Run("Should find violation for index", func(t *testing.T) {
 		t.Parallel()
 
 		tree := mustParse(t, "CREATE INDEX pgcheck_key ON pgcheck(id);")
 		require.Len(t, tree.Stmts, 1)
 
-		res, err := missingIndexIfNotExists(tree, testCode, testSlug, testHelp)
+		res, err := missingIfNotExists(tree, testCode, testSlug, testHelp)
 		require.NoError(t, err)
 		require.Len(t, res, 1)
 
@@ -98,42 +49,43 @@ func TestMissingIndexIfNotExists(t *testing.T) {
 		t.Parallel()
 
 		var b strings.Builder
-		b.WriteString("CREATE INDEX pgcheck_key ON pgcheck(id);")
+		b.WriteString("CREATE TABLE pgcheck (id integer PRIMARY KEY);\n")
 		b.WriteString("ALTER TABLE pgcheck RENAME COLUMN value TO new_value;\n")
-		b.WriteString("CREATE INDEX pgcheck_key_two ON pgcheck(id);")
+		b.WriteString("CREATE INDEX pgcheck_key ON pgcheck(id);\n")
 		tree := mustParse(t, b.String())
 		require.Len(t, tree.Stmts, 3)
 
-		res, err := missingIndexIfNotExists(tree, testCode, testSlug, testHelp)
+		res, err := missingIfNotExists(tree, testCode, testSlug, testHelp)
 		require.NoError(t, err)
 		require.Len(t, res, 2)
 
 		assert.EqualValues(t, 0, res[0].StmtStart)
-		assert.EqualValues(t, 93, res[1].StmtStart)
+		assert.EqualValues(t, 100, res[1].StmtStart)
 	})
 
-	t.Run("Should not return statements that have safe guard", func(t *testing.T) {
+	t.Run("Should not return statements that are safe", func(t *testing.T) {
 		t.Parallel()
 
 		var b strings.Builder
-		b.WriteString("CREATE INDEX IF NOT EXISTS pgcheck_key ON pgcheck(id);")
+		b.WriteString("CREATE TABLE IF NOT EXISTS pgcheck (id integer PRIMARY KEY);")
 		tree := mustParse(t, b.String())
 		require.Len(t, tree.Stmts, 1)
 
-		res, err := missingIndexIfNotExists(tree, testCode, testSlug, testHelp)
+		res, err := missingIfNotExists(tree, testCode, testSlug, testHelp)
 		require.NoError(t, err)
 		assert.Empty(t, res)
 	})
 
-	t.Run("Should not return statements when index is not named", func(t *testing.T) {
+	t.Run("Should find no violations", func(t *testing.T) {
 		t.Parallel()
 
 		var b strings.Builder
-		b.WriteString("CREATE INDEX ON pgcheck(id);")
+		b.WriteString("ALTER TABLE pgcheck RENAME COLUMN value TO new_value;\n")
+		b.WriteString("ALTER TABLE pgcheck RENAME COLUMN value TO new_value;\n")
 		tree := mustParse(t, b.String())
-		require.Len(t, tree.Stmts, 1)
+		require.Len(t, tree.Stmts, 2)
 
-		res, err := missingIndexIfNotExists(tree, testCode, testSlug, testHelp)
+		res, err := missingIfNotExists(tree, testCode, testSlug, testHelp)
 		require.NoError(t, err)
 		assert.Empty(t, res)
 	})
